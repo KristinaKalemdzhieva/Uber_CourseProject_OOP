866aca0a1c2d00c67b6a25cf37899e8b0b300987
KristinaKalemdzhieva <80347232+KristinaKalemdzhieva@users.noreply.github.com>
2023-06-19 00:30:18 +0300

    add final changes

--- Command3.cpp
+++ Command3.cpp
@@ -2,6 +2,14 @@
 
 bool Command3::execute(std::ostream& os, System* system)
 {
-	system->saveInfoInFile();
+	try
+	{
+		system->saveInfoInFile();
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cout << e.what() << std::endl << std::endl;
+	}
+	
 	return true;
 }
